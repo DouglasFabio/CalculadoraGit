6bc4dc87e06bae572bb1fbfacbe3161f460687a9
José Teste <teste14.teste14@hotmail.com>
2019-04-24 20:00:41 -0300

    Nova alteração visual

--- CalculadoraGit/Form1.Designer.cs
+++ CalculadoraGit/Form1.Designer.cs
@@ -28,13 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelN1 = new System.Windows.Forms.Label();
+            this.labelN2 = new System.Windows.Forms.Label();
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.labelOperador = new System.Windows.Forms.Label();
+            this.comboBoxOperador = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // labelN1
+            // 
+            this.labelN1.AutoSize = true;
+            this.labelN1.Location = new System.Drawing.Point(13, 13);
+            this.labelN1.Name = "labelN1";
+            this.labelN1.Size = new System.Drawing.Size(56, 13);
+            this.labelN1.TabIndex = 0;
+            this.labelN1.Text = "Número 1:";
+            // 
+            // labelN2
+            // 
+            this.labelN2.AutoSize = true;
+            this.labelN2.Location = new System.Drawing.Point(12, 39);
+            this.labelN2.Name = "labelN2";
+            this.labelN2.Size = new System.Drawing.Size(56, 13);
+            this.labelN2.TabIndex = 1;
+            this.labelN2.Text = "Número 2:";
+            // 
+            // txtN1
+            // 
+            this.txtN1.Location = new System.Drawing.Point(75, 10);
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(100, 20);
+            this.txtN1.TabIndex = 2;
+            // 
+            // txtN2
+            // 
+            this.txtN2.Location = new System.Drawing.Point(75, 36);
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(100, 20);
+            this.txtN2.TabIndex = 3;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(100, 101);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            // 
+            // labelOperador
+            // 
+            this.labelOperador.AutoSize = true;
+            this.labelOperador.Location = new System.Drawing.Point(12, 71);
+            this.labelOperador.Name = "labelOperador";
+            this.labelOperador.Size = new System.Drawing.Size(54, 13);
+            this.labelOperador.TabIndex = 6;
+            this.labelOperador.Text = "Operador:";
+            // 
+            // comboBoxOperador
+            // 
+            this.comboBoxOperador.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxOperador.FormattingEnabled = true;
+            this.comboBoxOperador.Items.AddRange(new object[] {
+            "Adição",
+            "Subtração",
+            "Multiplicação",
+            "Divisão"});
+            this.comboBoxOperador.Location = new System.Drawing.Point(75, 68);
+            this.comboBoxOperador.Name = "comboBoxOperador";
+            this.comboBoxOperador.Size = new System.Drawing.Size(100, 21);
+            this.comboBoxOperador.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(187, 139);
+            this.Controls.Add(this.comboBoxOperador);
+            this.Controls.Add(this.labelOperador);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.txtN1);
+            this.Controls.Add(this.labelN2);
+            this.Controls.Add(this.labelN1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelN1;
+        private System.Windows.Forms.Label labelN2;
+        private System.Windows.Forms.TextBox txtN1;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label labelOperador;
+        private System.Windows.Forms.ComboBox comboBoxOperador;
     }
 }
 
